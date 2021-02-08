afce43eff3ed09c29ff8df53e4fa7bd99ec15a35
Margaux RUSSEIL <margaux.russeil.19@gmail.com>
2021-02-08 10:19:29 +0100

    Initial Commit

---
+++ vin21/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vin21
+//
+//  Created by Margaux RUSSEIL on 08/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ vin21/vin21App.swift
@@ -0,0 +1,17 @@
+//
+//  vin21App.swift
+//  vin21
+//
+//  Created by Margaux RUSSEIL on 08/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct vin21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
